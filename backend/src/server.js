2277acd19a0c22cc29b67b2c112d6650e1287e09
Yagyesh <saraswatthrust@gmail.com>
2025-10-12 02:01:21 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,37 @@ import express from "express";
 import dotenv from "dotenv"; // to use .env file
 import authRoutes from "./routes/auth.route.js"; // .js bcos it is a local file
 import messageRoutes from "./routes/message.route.js"
+import path from "path"; // deployment
 
 
 dotenv.config(); // configure dotenv
 
 const app = express();
+const __dirname = path.resolve(); // deployment
 
 // to check if env file is working
 console.log(process.env.PORT); // O/P 3000
 const PORT = process.env.PORT || 3000;
 
+// app.use(PORT, (req, res) => {
+//     res.send("Hail Hitler")
+// }); // setting endpoint for messages
 app.use("/api/auth", authRoutes); // set endpoint for authentication
 app.use("/api/message", messageRoutes); // setting endpoint for messages
+
+
+// make ready for deployment
+if(process.env.NODE_ENV === "production") { // checking if we are in production
+
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))) // adding static assests
+
+    // for any other endpoints
+app.get("*", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+}
+
 
 
 
