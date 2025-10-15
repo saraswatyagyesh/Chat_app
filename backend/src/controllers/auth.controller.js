92871dffac8c2ec0e8c617c60f0343c0171a0aa6
Yagyesh <saraswatthrust@gmail.com>
2025-10-16 04:13:24 +0530

    add login and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -87,3 +87,43 @@ export const signup = async(req, res) => {
     
     // res.send("Signup endpoint")
 };
+
+export const login = async (req, res) => {
+
+    const {email, password} = req.body
+
+    try {
+        //  checking if user exists
+        const user = await User.findOne({email}) // checking it by email
+
+        // if user does not exist
+        if(!user) return res.status(400).json({message: "Invalid Credentials"});
+        // Never tell the client, the real deal
+
+        // checking password
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect) return res.status(400).json({message: "Invalid Credentials"});
+
+        // if both are incorrect we can generate token
+        generateToken(user._id, res)
+
+        // Sending the data
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+
+        console.error("Error in login controller: ", error);
+        res.status(500).json({message: "Internal server error"});
+        
+    }
+};
+
+export const logout = (_, res) => {
+
+    res.cookie("jwt", "", {maxAge:0})
+    res.status(200).json({message: "Logged out successs"});
+};
